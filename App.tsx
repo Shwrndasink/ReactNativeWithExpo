f6b5fd11b92f193d89e99650b1798b1f19e6c2d0
Marc White <marcwhite@Marcs-MacBook-Air.local>
2024-08-17 14:38:05 -0600

    adding some styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Here are some boxes of different colors
+        </Text>
+        <View style={styles.blackBoxWrapper}>
+          <Text style={styles.blackBox}>Black box with white text</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    backgroundColor: "lightblue",
+    borderWidth: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  blackBoxWrapper: {
+    flex: 1,
+  },
+  blackBox: {
+    color: "white",
+    backgroundColor: "black",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    flexDirection: "row",
+    marginTop: 15,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
